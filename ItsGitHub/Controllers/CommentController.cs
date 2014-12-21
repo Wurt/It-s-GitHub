9e51f76527765aa852f72913cfc6b30add5cdce7
Jordi <me@jordify.nl>
2014-12-21 05:18:14 +0100

    More work on comment support for assignments.

---
+++ ItsGitHub/Controllers/CommentController.cs
@@ -0,0 +1,29 @@
+﻿using ItsGitHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ItsGitHub.Controllers
+{
+    public class CommentController : Controller
+    {
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Comment comment)
+        {
+            db.Comment.Add(comment);
+            db.SaveChanges();
+
+            return RedirectToAction("Create");
+        }
+
+        private CommentContext db = new CommentContext();
+    }
+}
---
+++ ItsGitHub/Models/CommentContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ItsGitHub.Models
+{
+    public class CommentContext : DbContext
+    {
+        public DbSet<Comment> Comment { get; set; }
+    }
+}
