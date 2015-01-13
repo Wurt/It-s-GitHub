779bc8fa7bddd495a85179f9504e0880489d91db
Jordi <me@jordify.nl>
2015-01-13 17:58:59 +0100

    First few tidbits

---
+++ ItsGitHub/Controllers/AuthController.cs
@@ -0,0 +1,81 @@
+﻿using ItsGitHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ItsGitHub.Controllers
+{
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult LogIn(string returnUrl)
+        {
+            var model = new LogInViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(LogInViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            // Don't do this in production!
+            if (model.Email == "me@jordify.nl" && model.Password == "itsgithub")
+            {
+                var identity = new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.Name, "Jordi"),
+                new Claim(ClaimTypes.Email, "me@jordify.nl"),
+                new Claim(ClaimTypes.Country, "Netherlands")
+            },
+                    "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+
+                authManager.SignIn(identity);
+
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+
+            // user auth failed
+            ModelState.AddModelError("", "Invalid email or password");
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("index", "home");
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "home");
+            }
+
+            return returnUrl;
+        }
+    }
+}
+
