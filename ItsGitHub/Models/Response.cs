63bbde0bc798f184bdb736ecaa739a1f23591626
Jordi de Roo <Way2jordi@hotmail.com>
2015-01-20 10:45:03 +0100

    Made Datetime stamp for response

--- ItsGitHub/Models/Response.cs
+++ ItsGitHub/Models/Response.cs
@@ -10,6 +10,20 @@ namespace ItsGitHub.Models
         public int ID { get; set; }
         public string Content { get; set; }
         public int AssignmentId { get; set; }
+               public DateTime DateCreatedForResp
+        {
+            get
+            {
+                return this.dateCreated.HasValue
+                   ? this.dateCreated.Value
+                   : DateTime.Now;
+            }
+
+            set { this.dateCreated = value; }
+        }
+
+        private DateTime? dateCreated = null;
+    }
 
     }
-}
+
